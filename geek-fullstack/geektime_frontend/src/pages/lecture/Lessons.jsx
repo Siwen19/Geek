ffbd1a19fb138bad9ddb331e8bf4f480048a8146
Siwen <LiSiwen19@163.com>
2020-07-22 20:49:29 +0800

    update

--- geek-fullstack/geektime_frontend/src/pages/lecture/Lessons.jsx
+++ geek-fullstack/geektime_frontend/src/pages/lecture/Lessons.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Header from './header/Header';
 import Path from './learn-path/Path';
-import './Lessons.css';
+import {Tab} from './Lessons.style'; 
 import Direction from './lesson-direction/Direction';
 import AllLessons from './allLessons/allLessons';
 
@@ -13,11 +13,11 @@ export default function Lessons() {
             .then(res => setPathList(res));
     }, []) 
     return (
-        <div className="lessons_wrapper">
+        <Tab>
             <Header />
             <Path data={pathList.STUDY_PATH_TITLE} />
             <Direction data={pathList.LESSONS_DIRECTION_TITLE} />
             <AllLessons data={pathList.ALL_LESSONS_LESSONS}/>
-        </div>
+        </Tab>
     )
 }
