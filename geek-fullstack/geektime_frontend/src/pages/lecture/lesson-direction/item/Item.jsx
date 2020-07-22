5c133672c01c9bc6701efb1912ac549b8048a06f
Siwen <LiSiwen19@163.com>
2020-07-22 20:35:30 +0800

    delete

--- geek-fullstack/geektime_frontend/src/pages/lecture/lesson-direction/item/Item.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Item.css';
-
-export default function Item(props) { 
-    const {data} = props;
-    return (
-    <div className="K0VUHZTt_0">{data}</div>
-    )
-};
